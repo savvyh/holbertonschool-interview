449a6d739bde7613375bb5d704f83a2cf5d42af9
savvyh <7277@holbertonstudents.com>
2025-04-17 13:56:47 +0000

    fix: fix the method by checking the side index of the grid

--- island_perimeter/0-island_perimeter.py
+++ island_perimeter/0-island_perimeter.py
@@ -15,4 +15,6 @@ def island_perimeter(grid):
                 perimeter += 4
                 if i > 0 and grid[i - 1][j] == 1:
                     perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
     return perimeter
