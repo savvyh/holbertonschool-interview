eefe856cad6ebb2b93ac0d242c2f0eb9ffaf1aef
savvyh <7277@holbertonstudents.com>
2025-04-17 13:27:29 +0000

    feat: add a function to set if all boxes can be open with keys

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    """
+    Determine if all boxes can be opened with keys.
+    """
+    opened = set()
+    stack = [0]
+    opened.add(0)
+
+    while stack:
+        current_box = stack.pop()
+        for key in boxes[current_box]:
+            if key < len(boxes) and key not in opened:
+                opened.add(key)
+                stack.append(key)
+
+    return len(opened) == len(boxes)
