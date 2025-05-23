8161a8fa8276d44c76a775c41a89cf4700eb6693
savvyh <7277@holbertonstudents.com>
2025-05-23 09:36:29 +0000

    chore: improve method to get final result

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -16,4 +16,7 @@ def minOperations(n):
        for i in range (2, n):
           if n % i == 0:
              sum += i
+             n = n // i
+          else:
+             i += 1
        return sum
