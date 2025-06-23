dfb5e61e34efb59b049ba315c9f73916766a6098
savvyh <7277@holbertonstudents.com>
2025-06-23 08:13:50 +0000

    feat: add a method that determines if a given data set represents a valid UTF-8 encoding.

---
+++ utf8_validation/0-validate_utf8.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+UTF-8 Validation
+"""
+
+
+def validUTF8(data):
+    """
+    Check if a given data set represents a valid UTF-8 encoding.
+
+    Args:
+        data: List of integers representing bytes
+
+    Returns:
+        True if data is a valid UTF-8 encoding, else False
+    """
+    if data == []:
+        return True
+
+    continuation_number = 0
+
+    for byte in data:
+        binary_representation = format(byte, '08b')
+
+        if continuation_number == 0:
+            for i in range(8):
+                if binary_representation[i] == '1':
+                    continuation_number += 1
+                else:
+                    break
+
+            if continuation_number == 0:
+                continue
+
+            if continuation_number == 1 or continuation_number > 4:
+                return False
+
+            continuation_number -= 1
+
+        else:
+            if not binary_representation.startswith('10'):
+                return False
+            continuation_number -= 1
+
+    return continuation_number == 0
