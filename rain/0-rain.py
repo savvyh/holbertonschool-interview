ba2b6d7058c40b9eadc6c54d5b9f376017097c58
savvyh <7277@holbertonstudents.com>
2025-06-23 18:01:08 +0000

    feat: add a function to calculate water retained after rain

---
+++ rain/0-rain.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+0-rain.py
+"""
+
+
+def rain(walls):
+    """
+    Calculate how many square units of water will be retained after it rains.
+    """
+    if not walls:
+        return 0
+
+    walls_length = len(walls)
+    total_rainwater = 0
+
+    for i in range(walls_length):
+        max_left = max(walls[:i+1]) if i > 0 else walls[i]
+
+        max_right = max(walls[i:]) if i < walls_length-1 else walls[i]
+
+        water_at_position = min(max_left, max_right) - walls[i]
+
+        if water_at_position > 0:
+            total_rainwater += water_at_position
+
+    return total_rainwater
