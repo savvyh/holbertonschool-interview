a77c209ff74e7c263bade17821fd8c8f806c1e19
savvyh <7277@holbertonstudents.com>
2025-09-19 12:06:56 +0000

    fix: compilation fix

--- wild_cmp/0-wildcmp.c
+++ wild_cmp/0-wildcmp.c
@@ -18,7 +18,7 @@ int wildcmp(char *s1, char *s2)
     if (*s1 == *s2) {
         return (1);
     }
-    if (*s2 == '*' && *s1 = '*') {
+    if (*s2 == '*' && *s1 == '*') {
         return (1);
     }
     return (0);
