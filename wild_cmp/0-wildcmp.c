c3716ad026c420e5d69203d6c0c003c313b13498
savvyh <7277@holbertonstudents.com>
2025-09-19 12:00:12 +0000

    feat: add new comparison

--- wild_cmp/0-wildcmp.c
+++ wild_cmp/0-wildcmp.c
@@ -15,5 +15,11 @@ int wildcmp(char *s1, char *s2)
     if (*s1 == '\0' && *s2 == '*') {
         return (1);
     }
+    if (*s1 == *s2) {
+        return (1);
+    }
+    if (*s2 == '*' && *s1 == '*') {
+        return (1);
+    }
     return (0);
 }
