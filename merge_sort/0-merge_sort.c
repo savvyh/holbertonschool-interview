a87c10f42c07a4c20b441b77b6628e18f6a33ca6
savvyh <7277@holbertonstudents.com>
2026-03-06 13:34:28 +0100

    fix: betty style

--- merge_sort/0-merge_sort.c
+++ merge_sort/0-merge_sort.c
@@ -3,6 +3,88 @@
 #include <string.h>
 
 /**
+ * print_subarray - Prints a subarray of integers
+ *
+ * @array: The subarray to print
+ * @size: Number of elements in subarray
+ */
+void print_subarray(int *array, size_t size)
+{
+	size_t i;
+
+	if (size == 0)
+		return;
+	for (i = 0; i < size; i++)
+	{
+		if (i > 0)
+			printf(", ");
+		printf("%d", array[i]);
+	}
+}
+
+/**
+ * merge - Merges two sorted subarrays into one
+ *
+ * @array: The array containing both subarrays
+ * @left: Starting index of left subarray
+ * @mid: Ending index of left subarray
+ * @right: Ending index of right subarray
+ * @temp: Temporary array for merging
+ */
+void merge(int *array, size_t left, size_t mid, size_t right, int *temp)
+{
+	size_t i = left, j = mid + 1, k = left;
+
+	printf("Merging...\n");
+	printf("[left]: ");
+	print_subarray(array + left, mid - left + 1);
+	printf("\n");
+	printf("[right]: ");
+	print_subarray(array + j, right - j + 1);
+	printf("\n");
+
+	while (i <= mid && j <= right)
+	{
+		if (array[i] <= array[j])
+			temp[k++] = array[i++];
+		else
+			temp[k++] = array[j++];
+	}
+	while (i <= mid)
+		temp[k++] = array[i++];
+	while (j <= right)
+		temp[k++] = array[j++];
+
+	for (i = left; i <= right; i++)
+		array[i] = temp[i];
+
+	printf("[Done]: ");
+	print_subarray(array + left, right - left + 1);
+	printf("\n");
+}
+
+/**
+ * merge_sort_helper - Recursively splits array and merges
+ *
+ * @array: The array to sort
+ * @left: Starting index of current subarray
+ * @right: Ending index of current subarray
+ * @temp: Temporary array for merging
+ */
+void merge_sort_helper(int *array, size_t left, size_t right, int *temp)
+{
+	size_t mid;
+
+	if (left < right)
+	{
+		mid = left + (right - left) / 2;
+		merge_sort_helper(array, left, mid, temp);
+		merge_sort_helper(array, mid + 1, right, temp);
+		merge(array, left, mid, right, temp);
+	}
+}
+
+/**
  * merge_sort - Sorts an array of integers in ascending order
  *
  * @array: Array to sort
@@ -11,7 +93,6 @@
 void merge_sort(int *array, size_t size)
 {
 	int *temp;
-	size_t curr_size, left_start, mid, right_end, i, j, k, p;
 
 	if (array == NULL || size < 2)
 		return;
@@ -20,68 +101,6 @@ void merge_sort(int *array, size_t size)
 	if (temp == NULL)
 		return;
 
-	for (curr_size = 1; curr_size < size; curr_size *= 2)
-	{
-		for (left_start = 0; left_start < size; left_start += 2 * curr_size)
-		{
-			mid = left_start + curr_size - 1;
-			right_end = left_start + 2 * curr_size - 1;
-
-			if (mid >= size)
-				mid = size - 1;
-			if (right_end >= size)
-				right_end = size - 1;
-
-			if (mid < right_end)
-			{
-				i = left_start;
-				j = mid + 1;
-				k = left_start;
-
-				printf("Merging...\n");
-				printf("[left]: ");
-				for (p = left_start; p <= mid; p++)
-				{
-					if (p > left_start)
-						printf(", ");
-					printf("%d", array[p]);
-				}
-				printf("\n");
-				printf("[right]: ");
-				for (p = mid + 1; p <= right_end; p++)
-				{
-					if (p > mid + 1)
-						printf(", ");
-					printf("%d", array[p]);
-				}
-				printf("\n");
-
-				while (i <= mid && j <= right_end)
-				{
-					if (array[i] <= array[j])
-						temp[k++] = array[i++];
-					else
-						temp[k++] = array[j++];
-				}
-				while (i <= mid)
-					temp[k++] = array[i++];
-				while (j <= right_end)
-					temp[k++] = array[j++];
-
-				for (i = left_start; i <= right_end; i++)
-					array[i] = temp[i];
-
-				printf("[Done]: ");
-				for (p = left_start; p <= right_end; p++)
-				{
-					if (p > left_start)
-						printf(", ");
-					printf("%d", array[p]);
-				}
-				printf("\n");
-			}
-		}
-	}
-
+	merge_sort_helper(array, 0, size - 1, temp);
 	free(temp);
 }
