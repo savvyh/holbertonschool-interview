9d28129e2d293c1a6963c4f2293fd6d27031e408
savvyh <7277@holbertonstudents.com>
2026-03-06 09:59:29 +0100

    fix: betty style

--- menger/0-menger.c
+++ menger/0-menger.c
@@ -6,39 +6,42 @@
  * menger - Draws a 2D Menger Sponge
  * @level: The level of the sponge to draw
  */
-void menger(int level) {
-    if (level < 0) {
-        return;
-    }
-    
-    if (level == 0) {
-       printf("#\n");
-       return;
-    }
-    
-    int size = pow(3, level);
+void menger(int level)
+{
+	if (level < 0)
+		return;
 
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-            int x = i;
-            int y = j;
-            int is_empty = 0;
-            
-            while (x > 0 || y > 0) {
-                if (x % 3 == 1 && y % 3 == 1) {
-                    is_empty = 1;
-                    break;
-                }
-                x /= 3;
-                y /= 3;
-            }
-            
-            if (is_empty) {
-                printf(" ");
-            } else {
-                printf("#");
-            }
-        }
-        printf("\n");
-    }
+	if (level == 0)
+	{
+		printf("#\n");
+		return;
+	}
+
+	int size = pow(3, level);
+
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < size; j++)
+		{
+			int x = i;
+			int y = j;
+			int is_empty = 0;
+
+			while (x > 0 || y > 0)
+			{
+				if (x % 3 == 1 && y % 3 == 1)
+				{
+					is_empty = 1;
+					break;
+				}
+				x /= 3;
+				y /= 3;
+			}
+			if (is_empty)
+				printf(" ");
+			else
+				printf("#");
+		}
+		printf("\n");
+	}
 }
